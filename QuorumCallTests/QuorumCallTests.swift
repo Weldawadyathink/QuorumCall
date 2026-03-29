60731d6a3dd54e45174dbe107aed2aacbbd352d8
Spenser Bushey <spenser@spenserbushey.com>
2026-03-28 22:12:25 -0700

    Initial Commit

---
+++ QuorumCallTests/QuorumCallTests.swift
@@ -0,0 +1,17 @@
+//
+//  QuorumCallTests.swift
+//  QuorumCallTests
+//
+//  Created by Spenser Bushey on 28/3/26.
+//
+
+import Testing
+@testable import QuorumCall
+
+struct QuorumCallTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
